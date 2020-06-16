e2dc25b861db6de133d9dde435749955a0808f05
Ben <benmason1992@hotmail.co.uk>
2020-06-16 15:51:34 +0100

    made changes to page layout, updated font and background colour

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,20 +2,17 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://i.pinimg.com/originals/da/26/73/da26735ac703498f3fc8893f2e7751f5.jpg" style="height: 10rem;" class="rounded-circle">
+        </div>
+        
+        <div class="col-9 pt-5">
+            <div><h1>Official Account of Big Ben</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>3</strong> posts</div>
+                <div class="pr-5"><strong>1k</strong> followers</div>
+                <div class="pr-5"><strong>10</strong> following</div>
             </div>
         </div>
     </div>
