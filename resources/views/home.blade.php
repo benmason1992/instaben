cb5a5532e08c9f75550221dd664d7ddfad40f890
Ben <benmason1992@hotmail.co.uk>
2020-07-01 18:15:54 +0100

    added username to register, together with validation in model and controller

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,15 +6,32 @@
         <div class="col-3 p-5">
             <img src="https://i.pinimg.com/originals/da/26/73/da26735ac703498f3fc8893f2e7751f5.jpg" style="height: 10rem;" class="rounded-circle">
         </div>
-        
+
         <div class="col-9 pt-5">
-            <div><h1>Official Account of Big Ben</h1></div>
+            <div>
+                <h1>Official Account of Big Ben</h1>
+            </div>
             <div class="d-flex">
                 <div class="pr-5"><strong>3</strong> posts</div>
                 <div class="pr-5"><strong>1k</strong> followers</div>
                 <div class="pr-5"><strong>10</strong> following</div>
             </div>
+            <div class="pt-3">The worlds most famous clock tower bell, the name Big Ben is often used to describe the tower, the clock and the bell but the name was first given to the Great Bell. </div>
+            <div><a href="https://www.parliament.uk/bigben">https://www.parliament.uk/bigben</a></div>
         </div>
+    </div>
+
+    <div class="row pt-4">
+        <div class="col-4">
+            <img src="https://scontent-lhr8-1.cdninstagram.com/v/t51.2885-15/e35/c0.180.1440.1440a/s480x480/104395366_189978359010446_1974048144494651054_n.jpg?_nc_ht=scontent-lhr8-1.cdninstagram.com&_nc_cat=102&_nc_ohc=d5HJ7t-CxooAX_Gwvff&oh=61acc6eb8fe5ebbb4759b08af6241d69&oe=5F11EF31" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://scontent-lhr8-1.cdninstagram.com/v/t51.2885-15/e15/c0.135.1080.1080a/s480x480/104284387_156619055922175_5567580013300932140_n.jpg?_nc_ht=scontent-lhr8-1.cdninstagram.com&_nc_cat=107&_nc_ohc=KXEtFx9XTpsAX-DB8Ou&oh=9f047806efd7ee4a27c19c8b1ba12541&oe=5F124DF6" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://scontent-lht6-1.cdninstagram.com/v/t51.2885-15/e35/c0.92.1405.1405a/s320x320/103537699_173757220790924_8479651162927713151_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com&_nc_cat=108&_nc_ohc=zVO70Zz9Je0AX9Rpy1W&oh=62b7228e078dbdf62a12e1786824fbbc&oe=5F112077" class="w-100">
+        </div>
+
     </div>
 </div>
 @endsection
