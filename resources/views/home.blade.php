1bd219265e4b0f382e37a0b0e6e6d8de71d444ab
Ben <benmason1992@hotmail.co.uk>
2020-07-07 20:23:33 +0100

    set up foreign key between user and profile, also made changes to home.blade to pull through profile title, description and url

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,8 +16,9 @@
                 <div class="pr-5"><strong>1k</strong> followers</div>
                 <div class="pr-5"><strong>10</strong> following</div>
             </div>
-            <div class="pt-3">The worlds most famous clock tower bell, the name Big Ben is often used to describe the tower, the clock and the bell but the name was first given to the Great Bell. </div>
-            <div><a href="https://www.parliament.uk/bigben">https://www.parliament.uk/bigben</a></div>
+            <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+            <div class="pt-3">{{ $user->profile->description }}</div>
+            <div><a href="https://www.parliament.uk/bigben">{{ $user->profile->url }}</a></div>
         </div>
     </div>
 
