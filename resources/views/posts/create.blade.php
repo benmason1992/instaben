50431018d744d535b985f632dea84e28d3330f0e
Ben <benmason1992@hotmail.co.uk>
2020-07-07 22:12:37 +0100

    Added validation for caption and image.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,6 +3,8 @@
 @section('content')
 <div class="container">
     <form action="/p" enctype="multipart/form-data" method="post">
+        @csrf
+
         <div class="row">
             <div class="col-8 offset-2">
                 <div class="row">
@@ -12,7 +14,7 @@
                     <label for="caption" class="col-md-4 col-form-label">Post Caption</label>
 
 
-                    <input id="caption" type="text" class="form-control @error('caption') is-invalid @enderror" caption="caption" value="{{ old('caption') }}" required autocomplete="caption" autofocus>
+                    <input id="caption" type="text" class="form-control @error('caption') is-invalid @enderror" name="caption" value="{{ old('caption') }}" required autocomplete="caption" autofocus>
 
                     @error('caption')
                     <span class="invalid-feedback" role="alert">
@@ -26,11 +28,9 @@
                     <label for="image" class="col-md-4 col-form-label">Post Image</label>
                     <input type="file" , class="form-control-file" id="image" name="image">
 
-                    @error('image')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                    @enderror
+                    @if ($errors->has('image'))
+                        <strong>{{ $errors->first('image') }}</strong>
+                    @endif
                 </div>
 
                 <div class="row pt-4">
