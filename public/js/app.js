86143f386d0820cdb1c524cb81e9d0ca4a1bc1ef
Ben <benmason1992@hotmail.co.uk>
2020-08-03 12:11:11 +0100

    added controller for follow and used logic to return name of the profile user wants to follow as a placeholder

--- public/js/app.js
+++ public/js/app.js
@@ -1915,8 +1915,16 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 /* harmony default export */ __webpack_exports__["default"] = ({
+  props: ['userId'],
   mounted: function mounted() {
     console.log("Component mounted.");
+  },
+  methods: {
+    followUser: function followUser() {
+      axios.post('/follow/' + this.userId).then(function (response) {
+        alert(response.data);
+      });
+    }
   }
 });
 
@@ -37465,20 +37473,15 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _vm._m(0)
+  return _c("div", [
+    _c(
+      "button",
+      { staticClass: "btn btn-primary ml-4", on: { click: _vm.followUser } },
+      [_vm._v("Follow")]
+    )
+  ])
 }
-var staticRenderFns = [
-  function() {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c("div", [
-      _c("button", { staticClass: "btn btn-primary ml-4" }, [
-        _vm._v("Follow Me Now Please Work")
-      ])
-    ])
-  }
-]
+var staticRenderFns = []
 render._withStripped = true
 
 
