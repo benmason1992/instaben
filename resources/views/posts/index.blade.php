0794a5268d6c0f2abbc113411e1a25e66c3278dc
Ben <benmason1992@hotmail.co.uk>
2020-08-03 15:38:49 +0100

    have the followers post appearing in a index page for the user, most recent first

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,26 +5,16 @@
     @foreach($posts as $post)
 
     <div class="row">
-        <div class="col-8">
-            <img src="/storage/{{ $post->image }}" class="w-100">
+        <div class="col-6 offset-3">
+            <a href="/profile/{{ $post->user->id }}">
+                <img src="/storage/{{ $post->image }}" class="w-100">
+            </a>
         </div>
-        <div class="col-4">
-            <div>
-                <div class="d-flex align-items-center">
-                    <div class="pr-3">
-                        <img src="{{ $post->user->profile->profileImage() }}" class="w-100 rounded-circle" style="max-width: 40px;">
-                    </div>
-                    <div>
-                        <div class="font-weight-bold">
-                            <a href="/profile/{{ $post->user->id }}">
-                                <span class="text-dark">{{ $post->user->username }}</span>
-                            </a>
-                            <a href="#" class="pl-3">Follow</a>
-                        </div>
-                    </div>
-                </div>
+    </div>
 
-                <hr>
+    <div class="row pt-2 pb-4">
+        <div class="col-6 offset-3">
+            <div>
                 <p>
                     <span class="font-weight-bold">
                         <a href="/profile/{{ $post->user->id }}">
@@ -33,8 +23,9 @@
                     </span> {{ $post->caption }}</p>
             </div>
         </div>
+
     </div>
 
-    @endforeach
+@endforeach
 </div>
 @endsection
