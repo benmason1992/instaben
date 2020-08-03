9ef45849ca734b2122494cba679824d88078b742
Ben <benmason1992@hotmail.co.uk>
2020-08-03 11:51:19 +0100

    started working on adding vue component for following user

---
+++ resources/js/components/FollowButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <button class="btn btn-primary ml-4" @click="followUser">Follow Me Now Please Work</button>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log("Component mounted.");
+  }
+};
+</script>
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -11,7 +11,7 @@
             <div class="d-flex justify-content-between align-items-baseline">
                 <div class="d-flex align-items-center pb-2">
                     <h1 class="h4">{{ $user->username }}</h1>
-                    <button class="btn btn-primary ml-4">Follow</button>
+                    <follow-button></follow-button>
                 </div>
                 @can('update', $user->profile)
                     <a href="/p/create">Add new post</a>
