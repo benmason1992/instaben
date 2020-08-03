fff7ee0b945f29cf685fd63cede3e86e9d341cae
Ben <benmason1992@hotmail.co.uk>
2020-08-03 14:57:43 +0100

    installed laravel/telescope

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -20,8 +20,8 @@
             </div>
             <div class="d-flex">
                 <div class="pr-5"><strong>{{ $user->posts->count() }} </strong> posts</div>
-                <div class="pr-5"><strong>1k</strong> followers</div>
-                <div class="pr-5"><strong>10</strong> following</div>
+                <div class="pr-5"><strong>{{ $user->profile->followers->count() }}</strong> followers</div>
+                <div class="pr-5"><strong>{{ $user->following->count() }}</strong> following</div>
             </div>
             <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
             <div class="pt-3">{{ $user->profile->description }}</div>
