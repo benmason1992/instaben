561611ede4d6a5a33735cbde5283daf9e8bd445c
Ben <benmason1992@hotmail.co.uk>
2020-08-03 16:03:48 +0100

    added caching to the ProfilesController for followers, posts and following

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Intervention\Image\Facades\Image;
 
 
@@ -14,9 +15,29 @@ class ProfilesController extends Controller
     {
         $follows = (auth()->user()) ? auth()->user()->following->contains($user->id) : false;
 
-        $postCount = $user->posts->count();
-        $followerCount = $user->profile->followers->count();
-        $followingCount = $user->following->count();
+        $postCount = Cache::remember(
+            'count.posts.' . $user->id,
+            now()->addSeconds(30),
+            function () use ($user) {
+                return $user->posts->count();
+            }
+        );
+
+        $followerCount = Cache::remember(
+            'count.followers.' . $user->id,
+            now()->addSeconds(30),
+            function () use ($user) {
+                return $user->profile->followers->count();
+            }
+        );
+
+        $followingCount = Cache::remember(
+            'count.following.' . $user->id,
+            now()->addSeconds(30),
+            function () use ($user) {
+                return $user->following->count();
+            }
+        );
 
         return view('profiles.index', compact('user', 'follows', 'postCount', 'followerCount', 'followingCount'));
     }
@@ -48,10 +69,10 @@ class ProfilesController extends Controller
             $imageArray = ['image' => $imagePath];
         }
 
-        
+
 
         auth()->user()->profile->update(array_merge(
-            $data, 
+            $data,
             $imageArray ?? []
         ));
 
